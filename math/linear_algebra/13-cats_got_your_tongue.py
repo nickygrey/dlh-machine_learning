f513af8c265e38b837465c5465af252fe23be587
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-05-14 22:41:06 +0200

    Add numpy matrix concatenation

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that concatenates numpy arrays"""
+
+import numpy as np
+
+def np_cat(mat1, mat2, axis=0):
+    """Return concatenated matrices."""
+    return np.concatenate((mat1, mat2), axis=axis)
