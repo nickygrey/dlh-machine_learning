97ad7161dc6e07084b342e9a2db776f087927b91
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-05-14 22:30:27 +0200

    Add numpy element-wise operations

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Function that performs element-wise addition, subtraction, multiplication, and division"""
+"""Function that performs element-wise add, subt, mul, and div"""
 
 def np_elementwise(mat1, mat2):
-    """Return element-wise addition, subtraction, multiplication, and division of two matrices"""
+    """Return element-wise add, sub, mult, and div of two matrices"""
     return (mat1 + mat2,
             mat1 - mat2,
             mat1 * mat2,
