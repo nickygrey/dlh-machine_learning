da2ac11f1978cc825a7a7889dd2e90020e20e52d
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-06-05 08:29:33 +0200

    Implement task 3 dual line plot with legend

---
+++ math/plotting/3-two.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module to plot multiple radioactive decay functions."""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def two():
+    """Plot exponential decay of C-14 and Ra-226 simultaneously."""
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g', label='Ra-226')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.legend(loc='upper right')
+    plt.show()
