930eb2422d2e9ef7fe48b233c0bff1f0358c5e6e
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-06-09 15:19:35 +0200

    Implement task 1 Poisson PMF method with manual arithmetic

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -22,3 +22,26 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculate the value of the PMF for a given number of successes.
+
+        Args:
+            k (int): The number of successes.
+
+        Returns:
+            float: The PMF value for k.
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        pmf_val = ((self.lambtha ** k) * (e ** -self.lambtha)) / factorial
+        return pmf_val
