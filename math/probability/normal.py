9836b5e6acf7b9a9311ae9635626ee95cf841c01
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-06-11 10:00:50 +0200

    Implement task 7 Normal distribution Z-score and X-value

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -27,3 +27,25 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """Calculate the z-score of a given x-value.
+
+        Args:
+            x (float): The x-value.
+
+        Returns:
+            float: The z-score of x.
+        """
+        return float((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        """Calculate the x-value of a given z-score.
+
+        Args:
+            z (float): The z-score.
+
+        Returns:
+            float: The x-value of z.
+        """
+        return float(self.mean + (z * self.stddev))
