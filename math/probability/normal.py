01a30d613e109f7bb70cef8eb1b2d2bb7b6e401f
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-06-11 09:59:24 +0200

    Implement task 6 Normal distribution initialization

---
+++ math/probability/normal.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module that defines the Normal distribution class."""
+
+
+class Normal:
+    """Class that represents a normal distribution."""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initialize Normal distribution instance.
+
+        Args:
+            data (list): A list of data points to estimate the distribution.
+            mean (float): The mean of the distribution.
+            stddev (float): The standard deviation of the distribution.
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            self.mean = float(sum(data) / len(data))
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.stddev = float(variance ** 0.5)
