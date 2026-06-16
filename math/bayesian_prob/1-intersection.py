acb5e2cf95fa272640f12e87d192a2a43f819bfb
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-06-16 20:57:59 +0200

    Implement task 1 Bayesian Intersection with full error validation mapping

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Module to calculate the intersection of a binomial distribution."""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """Calculate intersection of data with hypothetical probabilities."""
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(P) is not np.ndarray or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
+        raise TypeError(
+            "Pr must be a numpy.ndarray with the same shape as P"
+        )
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+
+    num = 1
+    den = 1
+    for i in range(1, x + 1):
+        num *= (n - x + i)
+        den *= i
+    comb = num // den
+
+    likelihood = comb * (P ** x) * ((1 - P) ** (n - x))
+    intersect = likelihood * Pr
+
+    return intersect.astype(float)
