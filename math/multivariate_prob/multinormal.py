40fae20e57210c1109bf1b030a79fbc7324e15a7
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-07-03 11:24:56 +0200

    Update MultiNormal class with multivariate continuous PDF estimation method

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -24,3 +24,31 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         data_centered = data - self.mean
         self.cov = np.dot(data_centered, data_centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """Calculate the Probability Density Function (PDF) at a data point.
+
+        Args:
+            x (numpy.ndarray): A 2D array of shape (d, 1) containing
+                the data point.
+
+        Returns:
+            float: The value of the PDF at data point x.
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+
+        if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+
+        diff = x - self.mean
+        exponent = -0.5 * np.dot(np.dot(diff.T, inv), diff)
+        norm_coeff = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        pdf_val = norm_coeff * np.exp(exponent)
+
+        return float(pdf_val[0][0])
