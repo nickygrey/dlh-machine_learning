c53e9ecf979b554ee4a093231c6d792b36d29542
nickygrey <sgp5vb2qjd@privaterelay.appleid.com>
2026-07-08 09:46:52 +0200

    Implement task 1 initialization of a pandas DataFrame from a dictionary

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module to initialize a pandas DataFrame from a dictionary."""
+import pandas as pd
+
+data = {
+    "First": [0.0, 0.5, 1.0, 1.5],
+    "Second": ["one", "two", "three", "four"]
+}
+idx = ["A", "B", "C", "D"]
+
+df = pd.DataFrame(data, index=idx)
